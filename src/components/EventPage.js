e83d27925dbb6c671d12ee02ce3d9d68e43f47c3
Avihay Swissa <avihay126@gmail.com>
2024-09-25 11:08:44 +0300

    final update

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../styles/EventPage.css';
 
 function EventPage({ event }) {
     const [uploadProgress, setUploadProgress] = useState(0);
     const [images, setImages] = useState([]);
+    const [previewImages, setPreviewImages] = useState([]);
     const [message, setMessage] = useState("");
+    const [qrImage, setQrImage] = useState(null);
+    const [editMode, setEditMode] = useState(false);
 
-    // שמירת הקבצים שנבחרו
+    // הוספת דף נוכחי והגדרת כמות תמונות לדף
+    const [currentPage, setCurrentPage] = useState(1);
+    const imagesPerPage = 25;
+
+    useEffect(() => {
+        setUploadProgress(0);
+        setQrImage(null);
+        setMessage("");
+        setImages([]);
+        setPreviewImages([]);
+        setCurrentPage(1); // מאפסים את הדף הנוכחי בעת שינוי אירוע
+    }, [event]);
+
     const handleFileSelect = (e) => {
-        setImages([...e.target.files]);
-        setUploadProgress(0); // אפס את ההתקדמות לפני תחילת ההעלאה
+        const selectedFiles = Array.from(e.target.files);
+        setImages(selectedFiles);
+        const imagePreviews = selectedFiles.map((file) => URL.createObjectURL(file));
+        setPreviewImages(imagePreviews);
+        setUploadProgress(0);
+        setCurrentPage(1); // מאפסים את הדף הנוכחי בעת בחירת תמונות חדשות
     };
 
-    // תהליך ההעלאה לשרת
     const handleSubmit = () => {
-        debugger
         const formData = new FormData();
         images.forEach((image, index) => {
             formData.append(`image${index}`, image);
@@ -34,7 +51,7 @@ function EventPage({ event }) {
                 if (response.data.status === "success") {
                     setImages([]);
                     setMessage("התמונות עודכנו בהצלחה!");
-                    setUploadProgress(0)
+                    setUploadProgress(0);
                 } else {
                     setMessage("לא הצלחנו להעלות את התמונות. אנא נסה שנית!");
                 }
@@ -44,36 +61,138 @@ function EventPage({ event }) {
                 console.error("There was an error uploading the images!", error);
                 setMessage("אירעה שגיאה בהעלאה. אנא נסה שנית.");
             });
+    };
 
+    const handleQr = () => {
+        axios.get(`http://localhost:8000/get-qr?event-id=${event.id}`, { responseType: 'blob' })
+            .then((response) => {
+                const qrImageUrl = URL.createObjectURL(response.data);
+                setQrImage(qrImageUrl);
+            })
+            .catch((error) => {
+                console.error("Error fetching QR code:", error);
+            });
+    };
+
+    const closeQr = () => {
+        setQrImage(null);
+    };
+
+    const openEditMode = () => {
+        setEditMode(true);
+    };
+
+    const closeEditMode = () => {
+        setEditMode(false);
+    };
+
+    const removeImage = (index) => {
+        const newImages = images.filter((_, i) => i !== index);
+        const newPreviews = previewImages.filter((_, i) => i !== index);
+        setImages(newImages);
+        setPreviewImages(newPreviews);
+    };
+
+    // חישוב התצוגה של הדף הנוכחי
+    const indexOfLastImage = currentPage * imagesPerPage;
+    const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+    const currentImages = previewImages.slice(indexOfFirstImage, indexOfLastImage);
+
+    const nextPage = () => {
+        if (currentPage < Math.ceil(previewImages.length / imagesPerPage)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
     };
 
     return (
         <div className={"event-page-container"}>
             <div className={"event-page-title"}>
-                {event.name} - {event.date} - {event.location}
-            </div>
-            <div className={"event-page-body"}>
-                <h3>Upload Photos</h3>
-                <input
-                    type="file"
-                    className={"upload-input"}
-                    onChange={handleFileSelect}
-                    multiple
-                />
-                <div className="progress-bar">
-                    <div className="progress" style={{ width: `${uploadProgress}%` }}></div>
+                <h1 className="event-name">{event.name} {!event.is_open && <span>🔒</span>}</h1>
+                <div className="event-details">
+                    <span className="event-date">{event.date}</span>
+                    <span style={{ fontWeight: "bold" }}> · </span>
+                    <span className="event-location">{event.location}</span>
                 </div>
-                <button
-                    className={"upload-button"}
-                    onClick={handleSubmit}
-                    disabled={images.length ===0 || uploadProgress === 100}
-                >
-                    Upload
-                </button>
-                {message}
+                <button id={"qr-button"} className="event-button" onClick={handleQr}>QR</button>
             </div>
+
+            {
+                event.is_open ?
+                    <div className={"event-page-body"}>
+                        <h3>Upload Photos</h3>
+                        <input
+                            type="file"
+                            className={"upload-input"}
+                            onChange={handleFileSelect}
+                            multiple
+                        />
+                        <button
+                            className={"event-button"}
+                            onClick={openEditMode}
+                            disabled={images.length === 0}
+                        >
+                            Edit
+                        </button>
+
+                        <div className="progress-bar">
+                            <div className="progress" style={{ width: `${uploadProgress}%` }}></div>
+                        </div>
+                        <button
+                            className={"upload-button"}
+                            onClick={handleSubmit}
+                            disabled={images.length === 0 || uploadProgress === 100}
+                        >
+                            Upload
+                        </button>
+                        {message}
+                    </div>
+                    :
+                    <div className={"centered-message"}>
+                        Event locked, no more photos can be uploaded
+                    </div>
+            }
+
+            {qrImage && (
+                <div className="qr-code-popup">
+                    <span className="close" onClick={closeQr}>&times;</span>
+                    <img src={qrImage} alt="QR Code" className="qr-image" />
+                    <a href={qrImage} download={`${event.name}_qr_code.jpg`}>Download QR Code</a>
+                </div>
+            )}
+
+            {editMode && (
+                <div className="edit-popup">
+                    <div className="edit-content">
+                        <h3>Edit Selected Images</h3>
+                        <div className="image-preview-container">
+                            {currentImages.map((image, index) => (
+                                <div key={index} className="image-preview">
+                                    <img src={image} alt={`Selected ${index}`} />
+                                    <button className="remove-button" onClick={() => removeImage(index)}>X</button>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="pagination">
+                            <button className="pagination-button" onClick={prevPage} disabled={currentPage === 1}>
+                                Previous
+                            </button>
+                            <span>Page {currentPage}</span>
+                            <button className="pagination-button" onClick={nextPage} disabled={currentPage === Math.ceil(previewImages.length / imagesPerPage)}>
+                                Next
+                            </button>
+                        </div>
+                        <button className="save-button" onClick={closeEditMode}>Save</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
-};
+}
 
 export default EventPage;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -34,31 +34,74 @@ const Profile = () => {
     const getEvents =()=>{
         axios.get("http://localhost:8000/get-events",{withCredentials: true})
             .then((response)=>{
-
+                debugger
                 setEvents(response.data)
             })
 
     }
 
-    //פונקציה למעבר לדף האירוע
-    const handleEventClick = (event) => {
-        navigate(`/event/${event.name}-${event.location}`, { state: { event } });
+
+    const sortEventsByDate = (events) => {
+        return events.sort((a, b) => {
+            const [dayA, monthA, yearA] = a.date.split('/').map(Number);
+            const [dayB, monthB, yearB] = b.date.split('/').map(Number);
+
+            const dateA = new Date(yearA, monthA - 1, dayA); // month is 0-indexed
+            const dateB = new Date(yearB, monthB - 1, dayB);
+
+            return dateA - dateB; // Sort in ascending order
+        });
+    };
+
+    const handleLockEvent =()=>{
+        const confirmed = window.confirm("Are you sure you want to lock this event? This action cannot be undone.");
+        if (confirmed){
+            axios.get("http://localhost:8000/lock-event?event-id="+selectedEvent.id)
+                .then((response)=>{
+                    console.log(response.data)
+                    debugger
+                    updateLockedEvent(response.data.event)
+                    setSelectedEvent(response.data.event)
+
+                })
+        }
+
+    }
+
+    const updateLockedEvent = (event) => {
+        setEvents(prevEvents =>
+            prevEvents.map(ev => ev.id === event.id ? event : ev)
+        );
     };
 
 
     return (
         <div className={"main-container"}>
             <div className={"profile-header-container"}>
-                {user.name} {user.email} {user.phone}
+                <h1 className="event-name">{user.name}</h1>
+                <div id={"edit-user-button"}>🖉</div>
+                <div className="event-details">
+                    <span className="event-date">{user.phone}</span>
+                    <span style={{ fontWeight: "bold" }}> · </span>
+                    <span className="event-location">{user.email}</span>
+                </div>
             </div>
             <button id={"create-event"} className={"event-button"} onClick={() => setShowModal(true)}>Create Event</button>
-            <MyEvents events={events} selectedEvent={selectedEvent} setSelectedEvent={setSelectedEvent}/>
+
+            <MyEvents events={sortEventsByDate(events)} selectedEvent={selectedEvent} setSelectedEvent={setSelectedEvent}/>
             {showModal && (
                <CreateEvent setShowModal={setShowModal} setEvents={setEvents}/>
             )}
             {
                 selectedEvent !== null &&
-                <EventPage event={selectedEvent}/>
+                <div>
+                    {
+                        selectedEvent.is_open &&
+                        <button id={"lock-event"} className={"event-button"} onClick={handleLockEvent}>Lock Event</button>
+                    }
+                    <EventPage event={selectedEvent}/>
+                </div>
+
             }
 
         </div>
